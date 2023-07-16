8d997f61828d03eefc07c9880bd25003873f0a8d
Syed Faraz <farazshah116@gmail.com>
2023-07-16 15:01:45 +0500

    fix: cleaning code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,68 +1,7 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { login, logout } from "../store/reducers/authenticationSlice";
-import { useSelector } from "react-redux";
-
-// User login component
 
 const Home = () => {
-  const dispatch = useDispatch();
-  const isAuthenticated = useSelector(
-    (state) => state.authentication.isAuthenticated
-  );
-  const role = useSelector((state) => state.authentication.role);
-
-  const UserLogin = () => {
-    const handleLogin = () => {
-      // Simulate form input
-      const username = "user";
-      const password = "user123";
-
-      dispatch(login({ username, password }));
-    };
-
-    return <button onClick={handleLogin}>User Login</button>;
-  };
-
-  // Admin login component
-  const AdminLogin = () => {
-    const dispatch = useDispatch();
-
-    const handleLogin = () => {
-      // Simulate form input
-      const username = "admin";
-      const password = "admin123";
-
-      dispatch(login({ username, password }));
-    };
-
-    return <button onClick={handleLogin}>Admin Login</button>;
-  };
-
-  const Logout = () => {
-    const dispatch = useDispatch();
-
-    const handleLogout = () => {
-      dispatch(logout());
-    };
-
-    return <button onClick={handleLogout}>Logout</button>;
-  };
-
-  if (!isAuthenticated) {
-    return <div>Please log in.</div>;
-  }
-
-  return (
-    <>
-      <h2>User Profile</h2>
-      <p>Role: {role}</p>
-      <Logout />
-
-      <AdminLogin />
-      <UserLogin />
-    </>
-  );
+  return <>{/* {example home components} */}</>;
 };
 
 export default Home;
